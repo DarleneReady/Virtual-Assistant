d140a8a722f749745d7fd352f3837dd38dc68ca5
DarleneReady <darlene_ready@yahoo.com>
2021-03-17 18:32:27 -0400

    Added Hamburger Bar, Form, Resume, Skills

---
+++ Form.js
@@ -0,0 +1,20 @@
+<form-action="Contact Me"; method="post">
+ <ul>
+  <li>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="user_name">
+  </li>
+  <li>
+    <label for="mail">E-mail:</label>
+    <input type="email" id="mail" name="user_email">
+  </li>
+  <li>
+    <label for="tasks">Tasks Needed</label>
+    <textarea id="msg" name="user_message"></textarea>
+  </li>
+ </ul>
+</form>
+
+<li class="button">
+  <button type="submit">Submit</button>
+</li class>
---
+++ navMenu.js
@@ -0,0 +1,19 @@
+var menuButton = document.getElementBYID("menuButton");
+
+function openNavMenu()
+    menuButton.addEventListener('click', function open(){
+      let navMenu= document.getElementById('nav-menu');
+
+      navMenu.style(display-flex)
+      .console.log(navMenu);
+      navMenu.style.display = "flex";
+
+    })
+
+
+function closeNavMenu(){
+    menuButton.addEventListener('click')
+        
+    }
+
+openNavMenu();

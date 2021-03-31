273a6dc73e8fc851f11dd46559c4da1e337f503a
DarleneReady <77470104+DarleneReady@users.noreply.github.com>
2021-03-30 20:10:29 -0400

    Update navMenu.js

--- navMenu.js
+++ navMenu.js
@@ -1,19 +1,24 @@
-var menuButton = document.getElementBYID("menuButton");
+var menu = document.querySelector(".menu");
+var ham = document.querySelector(".ham");
+var xIcon = document.querySelector(".xIcon");
+var menuIcon = document.querySelector(".menuIcon");
+ham.addEventListener("click", toggleMenu);
 
-function openNavMenu()
-    menuButton.addEventListener('click', function open(){
-      let navMenu= document.getElementById('nav-menu');
-
-      navMenu.style(display-flex)
-      .console.log(navMenu);
-      navMenu.style.display = "flex";
-
-    })
-
-
-function closeNavMenu(){
-    menuButton.addEventListener('click')
-        
-    }
-
-openNavMenu();
+menu.classList.contains("showMenu");
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+var menuLinks = document.querySelectorAll(".menuLink");
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleMenu);
+  }
+)
